e095131db3049cd22d257fb1e7bff008c120dd96
valmsmith39a <valmsmith39a@gmail.com>
2016-01-11 19:42:23 -0800

    First commit

--- main.js
+++ main.js
@@ -2,17 +2,47 @@
 
 $(document).ready(init);
 
-function(init) {
+function init() {
 
-$('body').append('<div id=\'testDiv\'>TEST</div>');
+// var $testDiv = $('<div id=\'testDiv\'>TEST</div>');
 
-$('#testDiv').click(clickedSquare);
+// $('body').append('<div id=\'testDiv\'>hello world</div>');
 
+setUpGame();
+
+}
+
+function setUpGame() {
+    
+	console.log('set up game');
+    
+    var arrayOfSquares = [];
+
+    for(var i = 0; i < 9; i++) {
+    	var $squares = $('<div>').addClass('squares').data('player', i);
+    	arrayOfSquares.push($squares);
+    }
+
+    $('.main-container').append(arrayOfSquares);
+
+    $('.squares').click(clickedSquare);
+
+    determineStartingPlayer();
+}
+
+function determineStartingPlayer() {
+	
 }
 
 function clickedSquare() {
 
 	console.log('someone clicked me!');
+
+	var $this = $(this)
+
+	$this.text('X');
+
+	$this.data('player', 1)
 
 }
 
